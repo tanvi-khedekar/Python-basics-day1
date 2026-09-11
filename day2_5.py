c833f1afd2ad1c5e4c6bf81b88ba4cf6926a4453
tanvi-khedekar <tanvi.khedekar125@gmail.com>
2026-09-11 22:33:54 +0530

    conditional operators in python

---
+++ day2_5.py
@@ -0,0 +1,38 @@
+#write a code to 
+
+#age = input("Enter age: ")
+#if int(age) > 18:
+#    print("Eligible")
+# else:
+#     print("Not eligible")
+
+
+# #check if number is even
+
+# number = input("Enter a number: ")
+# if int(number) % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#code to print output eg sunday for input 6, monday for input 0
+
+day = input("Enter number between 0-6 to check day of the week: ")
+if int(day) == 0:
+    print("MONDAY")
+elif int(day) == 1:
+    print("TUESDAY")
+elif int(day) == 2:
+    print("WEDNESDAY")
+elif int(day) == 3:
+    print("THURSDAY")
+elif int(day) == 4:
+    print("FRIDAY")
+elif int(day) == 5:
+    print("SATURDAY")
+elif int(day) == 6:
+    print("SUNDAY")
+else:
+    print("ATHAVDYAT 7ACH DIVAS ASTAT YEDYA")
+
+#MATCH CASES IN PYTHON
